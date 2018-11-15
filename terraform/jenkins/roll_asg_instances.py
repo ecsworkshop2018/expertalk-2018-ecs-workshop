69cd834d7498d8d385e3d35991b0f897486278e1
Prashant Kalkar <kalkar.prashant@gmail.com>
2018-11-15 13:18:14 +0530

    Added script to roll ASG instances.

---
+++ terraform/jenkins/roll_asg_instances.py
@@ -0,0 +1,96 @@
+import sys
+import time
+import logging
+import boto3
+
+IN_SERVICE_TIMEOUT = 600
+IN_SERVICE_INTERVAL = 20
+
+session = boto3.session.Session(profile_name='ecs-workshop')
+client = session.client('autoscaling')
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+def roll_instances(asg_name):
+    logger.debug("starting instance rolling for asg: %s", asg_name)
+    asg_info = client.describe_auto_scaling_groups(AutoScalingGroupNames=[asg_name])["AutoScalingGroups"][0]
+    asg = ASG(asg_name, instance_ids(asg_info), asg_info["DesiredCapacity"], asg_info["MaxSize"])
+    asg.roll_instances()
+    return
+
+
+def instance_ids(asg_info):
+    return map(lambda instance_info: instance_info["InstanceId"], asg_info["Instances"])
+
+
+def suspend_processes(asg_name):
+    logger.debug("suspending scaling processes for asg: %s", asg_name)
+    client.suspend_processes(AutoScalingGroupName=asg_name, ScalingProcesses=["HealthCheck", "ReplaceUnhealthy",
+                                                                              "AZRebalance", "AlarmNotification",
+                                                                              "ScheduledActions"])
+    return
+
+
+def update_asg_desired_max(asg_name, desired, max):
+    logger.debug("updating asg %s desired count to %d and max %d", asg_name, desired, max)
+    client.update_auto_scaling_group(AutoScalingGroupName=asg_name, DesiredCapacity=desired, MaxSize=max)
+    return
+
+
+def in_service_instances(asg_name):
+    asg_info = client.describe_auto_scaling_groups(AutoScalingGroupNames=[asg_name])
+    instance_lifecycle_states = map(lambda instance_info : instance_info["LifecycleState"], asg_info["AutoScalingGroups"][0]["Instances"])
+    number_of_in_service_instances = len([status for status in instance_lifecycle_states if status == "InService"])
+    logger.debug("number of instances in service %d for Asg: %s", number_of_in_service_instances, asg_name)
+    return number_of_in_service_instances
+
+
+def resume_processes(asg_name):
+    logger.debug("resuming scaling processes for asg: %s", asg_name)
+    client.resume_processes(AutoScalingGroupName=asg_name, ScalingProcesses=["HealthCheck", "ReplaceUnhealthy",
+                                                                              "AZRebalance", "AlarmNotification",
+                                                                              "ScheduledActions"])
+    return
+
+
+def terminate_instance(instance_id, decrement_desired_capacity):
+    logger.debug("terminating instance: %s", instance_id)
+    client.terminate_instance_in_auto_scaling_group(InstanceId=instance_id, ShouldDecrementDesiredCapacity=decrement_desired_capacity)
+    return
+
+
+class ASG:
+    def __init__(self, name, instances, desired, max):
+        self.name = name
+        self.instances = instances
+        self.desired = desired
+        self.max = max
+        return
+
+    def roll_instances(self, suspend_processes=suspend_processes, update_asg_desired_max=update_asg_desired_max, in_service_instances=in_service_instances, resume_processes=resume_processes, terminate_instance=terminate_instance):
+        suspend_processes(self.name)
+        update_asg_desired_max(self.name, self.desired*2, max(self.max, self.desired*2))
+
+        x = IN_SERVICE_TIMEOUT
+        while (x > 0) and (in_service_instances(self.name) < self.desired*2):
+            x -= IN_SERVICE_INTERVAL
+            time.sleep(IN_SERVICE_INTERVAL)
+
+        if in_service_instances(self.name) < self.desired*2:
+            resume_processes(self.name)
+            logger.error("TimeoutError: Enough instances did not become InService")
+            raise ValueError("TimeoutError: Enough instances did not become InService")
+
+        for i in self.instances:
+            terminate_instance(i, True)
+
+        update_asg_desired_max(self.name, self.desired, self.max)
+        resume_processes(self.name)
+        return
+
+
+if __name__ == "__main__":
+    asg_name = sys.argv[1]
+    roll_instances(asg_name)
---
+++ terraform/jenkins/tests/test_asg_instance_rolling.py
@@ -0,0 +1,120 @@
+import pytest
+
+from roll_asg_instances import ASG
+import roll_asg_instances
+
+roll_asg_instances.IN_SERVICE_TIMEOUT = 2
+roll_asg_instances.IN_SERVICE_INTERVAL = 1
+
+
+def test_should_suspend_scaling_processes_before_instance_rotation():
+    processes_suspended = [False]
+
+    def suspend_processes(asg_name):
+        assert asg_name == "asg"
+        processes_suspended[0] = True
+
+    # Given
+    asg = ASG("asg", ["i-4ba0837f"], 1, 5)
+    asg.roll_instances(suspend_processes, dummy_update_asg_desired_max, dummy_in_service_instances)
+    assert processes_suspended[0]
+
+
+def test_should_double_number_of_instances():
+    number_of_calls = [0]
+
+    def update_asg(asg_name, desired, max):
+        if number_of_calls[0] == 0:
+            assert asg_name == "asg"
+            assert desired == 2
+            assert max == 5
+        number_of_calls[0] += 1
+
+    asg = ASG("asg", ["i-4ba0837f"], 1, 5)
+    asg.roll_instances(dummy_suspend_processes, update_asg_desired_max=update_asg,
+                       in_service_instances=dummy_in_service_instances)
+
+
+def test_should_fail_if_instances_does_not_become_healthy_for_given_time():
+    asg = ASG("asg", ["i-4ba0837f"], 1, 5)
+    with pytest.raises(ValueError):
+        asg.roll_instances(dummy_suspend_processes, dummy_update_asg_desired_max, timeout_in_service_instances)
+
+
+def test_should_resume_processes_before_in_service_timeout():
+    processes_resumed = [False]
+
+    def resume_processes(asg_name):
+        processes_resumed[0] = True
+
+    asg = ASG("asg", ["i-4ba0837f"], 1, 5)
+    with pytest.raises(ValueError):
+        asg.roll_instances(dummy_suspend_processes, dummy_update_asg_desired_max, timeout_in_service_instances,
+                           resume_processes)
+    assert processes_resumed[0]
+
+
+def test_should_terminate_old_instances():
+    number_of_times_called = [0]
+
+    def terminate_instance(instance_id, decrement_desired_capacity):
+        assert instance_id == "i-4ba0837f"
+        assert decrement_desired_capacity
+        number_of_times_called[0] += 1
+
+    asg = ASG("asg", ["i-4ba0837f"], 1, 5)
+    asg.roll_instances(dummy_suspend_processes, dummy_update_asg_desired_max, dummy_in_service_instances,
+                       dummy_resume_processes, terminate_instance)
+    assert number_of_times_called[0] == 1
+
+
+def test_reset_desired_capacity_and_max_capacity():
+    number_of_calls = [0]
+
+    def update_asg(asg_name, desired, max):
+        if number_of_calls[0] == 1:
+            assert asg_name == "asg"
+            assert desired == 1
+            assert max == 5
+        number_of_calls[0] += 1
+
+    asg = ASG("asg", ["i-4ba0837f"], 1, 5)
+    asg.roll_instances(dummy_suspend_processes, update_asg, dummy_in_service_instances,
+                       dummy_resume_processes, dummy_terminate_instance)
+    assert number_of_calls[0] == 2
+
+
+def test_resume_services_after_instance_rolling():
+    processes_resumed = [False]
+
+    def resume_processes(asg_name):
+        processes_resumed[0] = True
+
+    asg = ASG("asg", ["i-4ba0837f"], 1, 5)
+    asg.roll_instances(dummy_suspend_processes, dummy_update_asg_desired_max, dummy_in_service_instances,
+                       resume_processes, dummy_terminate_instance)
+    assert processes_resumed[0]
+
+
+def dummy_suspend_processes(asg_name):
+    return
+
+
+def dummy_update_asg_desired_max(asg_name, desired, max):
+    return
+
+
+def dummy_in_service_instances(asg_name):
+    return 2
+
+
+def dummy_resume_processes(asg_name):
+    return
+
+
+def timeout_in_service_instances(asg_name):
+    return 1
+
+
+def dummy_terminate_instance(instance_id, decrease_desired_capacity):
+    return
