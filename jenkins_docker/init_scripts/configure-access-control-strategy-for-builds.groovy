bb7072de79b45095baddcdce1c6a87bcd8eb7a73
Kaushik Chandrashekar <kaushik.c02@gmail.com>
2018-11-20 13:36:56 +0530

    Update access control for jenkins to specific user

--- jenkins_docker/init_scripts/configure-access-control-strategy-for-builds.groovy
+++ jenkins_docker/init_scripts/configure-access-control-strategy-for-builds.groovy
@@ -3,11 +3,11 @@
 import jenkins.model.Jenkins
 import jenkins.security.QueueItemAuthenticatorConfiguration
 import org.jenkinsci.plugins.authorizeproject.GlobalQueueItemAuthenticator
-import org.jenkinsci.plugins.authorizeproject.strategy.TriggeringUsersAuthorizationStrategy
+import org.jenkinsci.plugins.authorizeproject.strategy.SpecificUsersAuthorizationStrategy
 
 def instance = Jenkins.getInstance()
 
-GlobalQueueItemAuthenticator auth = new GlobalQueueItemAuthenticator(new TriggeringUsersAuthorizationStrategy());
+GlobalQueueItemAuthenticator auth = new GlobalQueueItemAuthenticator(new SpecificUsersAuthorizationStrategy("JENKINS_USER_NAME"));
 QueueItemAuthenticatorConfiguration.get().getAuthenticators().add(auth);
 
 instance.save()
